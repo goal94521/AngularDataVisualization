9602eb05baa52c449bf89d254bde199b30eb8d53
Andy <andy.nikolenko@gmail.com>
2020-04-10 22:30:13 +0300

    multi-level node handling (left clikc - right click), links between nodes, tooltips new version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,8 @@ interface Node {
 }
 
 interface Link {
-  source: string;
-  target: string;
+  source: any;
+  target: any;
   value: number;
 }
 
@@ -49,46 +49,85 @@ export class AppComponent implements OnInit {
   link: any;
   graph: any;
   simulation: any;
+  state : any;
+  tooltip: any;
+  tooltipFields: any;
 
-
-   nodes: Node[] = [];
-   links: Link[] = [];
+  parentNodes: Node[] = [];
+  nodes: Node[] = [];
+  links: Link[] = [];
 
 
   constructor() {
-
-    this.currentLevel = this.levels[this.currentLevelIndex];
-
+    this.currentLevel = this.levels[this.currentLevelIndex];      
   }  
 
 
-  render( ) {
+  draw( ) {
 
     const simulation = this.simulation;
     const nodes = this.nodes;
     const links = this.links;
        
     const graph:Graph = <Graph>{ nodes, links };
+
+    console.log(graph.nodes);
+
     const that = this;
 
     const link = this.linkBase
       .selectAll('.links')
         .data(graph.links);
 
+    link.exit().transition().duration(500)
+        .style('opacity',0)
+        .remove();        
+
     const linkEnter = link   
         .enter()
         .append('line')
         .classed('links', true)
+        .style('opacity',0)
+        
+    linkEnter.transition().duration(500)
+      .style('opacity',1)
+    
+    const allLinks = linkEnter.merge(link);
 
     const node = this.nodeBase
       .selectAll(".nodes").data(graph.nodes, d => d.id);
 
-    node.exit().remove();
+    node.exit().transition().duration(500)
+      .style('opacity',0)
+      .remove();
 
     const nodeEnter = node.enter()  
       .append("g")
       .attr("class", "nodes")
-      .attr("id", (d: any) => {console.log(d); return "node-" + d.id } )
+      .style('opacity',0)
+      .attr("id", (d: any) => { 
+        //console.log('Node created:' + d.id); 
+          return  "node-" + d.id })    
+      .on("mousemove", (d) => {
+        this.tooltip
+            .style("left",d3.event.pageX + 20 + "px")
+            .style("top",d3.event.pageY  -10 + "px")
+
+        })
+      .on("mouseover", (d) => {
+        if(!this.tooltipFields) return;
+        this.tooltip
+            .html( that.tooltipFunc(d) )
+            .transition()
+            .duration(500)
+            .style("opacity", 1)
+        })
+      .on("mouseout", (d) => {
+        this.tooltip
+            .transition()
+            .duration(400)
+            .style("opacity", 0)
+        })
       .call(
         d3.drag()
             .on("start", (d: any) => {
@@ -96,7 +135,7 @@ export class AppComponent implements OnInit {
                     that.simulation.alphaTarget(0.01).restart()
                 d.fx = d.x
                 d.fy = d.y
-                d.movable = true
+               // d.movable = true
             })
             .on("drag",(d: any) => {
                 d.fx = d3.event.x
@@ -121,13 +160,22 @@ export class AppComponent implements OnInit {
         .on('mouseout', (d: any) => {
           const el = d3.select(d3.event.currentTarget);
           el.transition().duration(300).attr('r', this.radius - d.level *5);
-        }).on('click', (d: any ,i) => {
-          const el = d3.select(d3.event.currentTarget);
+        })
+        .on('click', (d: Node ,i) => {
           this.currentLevelIndex++;
-          this.nodes = [d];
-          this.loadData('assets/data.json');
-          
-        });
+          this.parentNodes.push(d);
+          this.loadData('assets/data.json');     
+          simulation.alpha(0.5).restart();     
+        })
+        .on("contextmenu", (d: any) => {
+          if(this.currentLevelIndex) {
+            this.currentLevelIndex--;
+            this.parentNodes.pop();
+            this.loadData('assets/data.json');       
+            simulation.alpha(0.5).restart();
+          }         
+          d3.event.preventDefault();          
+        })
 
 
     nodeEnter  
@@ -135,26 +183,26 @@ export class AppComponent implements OnInit {
         .text(d => d.Name)
         .attr('dy',5)
 
-    nodeEnter.merge(node)
+    nodeEnter.transition().duration(500).style('opacity',1)
+
+    const allNodes = nodeEnter.merge(node);
 
     simulation.nodes(graph.nodes)
-      .on('tick', ticked);
+        .on('tick', ticked)
+        .force("link",d3.forceLink().links(graph.links).strength(0.01));
 
-    const forceLink = d3.forceLink().links(graph.links).strength(0.01)
-
-    simulation.force("link",forceLink);
+    
 
 
     function ticked() {
-  
 
-      linkEnter  
+      allLinks  
         .attr('x1', function(d: any) { return d.source.x; })
         .attr('y1', function(d: any) { return d.source.y; })
         .attr('x2', function(d: any) { return d.target.x; })
         .attr('y2', function(d: any) { return d.target.y; });
   
-      nodeEnter
+      allNodes
         .attr(
           "transform",
             d => "translate(" +  ( 
@@ -162,61 +210,77 @@ export class AppComponent implements OnInit {
                 d.x = Math.max(d.size, Math.min(that.width - d.size, d.x)),
                 d.y = Math.max(d.size, Math.min(that.height - d.size, d.y))
             ] ) +
-
          ")"
     )
 
   }
    
-   // simulation.force<d3.ForceLink<any, any>>('link')
-   // .links(graph.links);
 
   }
 
   processData(error: any, alldata: any) {
 
 
-    console.log(alldata);
 
     const currentLevel = this.levels[this.currentLevelIndex];
     const data = alldata[currentLevel];
+    if(!data) return false;
     let index = 1;
+    this.links = [];
+    this.nodes = [];
 
+    if(this.parentNodes.length) {
+      this.nodes.push(this.parentNodes[this.parentNodes.length-1]);
+    }
+
+    
     data.forEach((d) => {
+      d.id = currentLevel + '_' + index++;
+      if(this.nodes[0] && this.nodes[0].id == d.id) return;
+
+      if(d._tooltipFields) {     
+        this.tooltipFields = d._tooltipFields;
+      }
       d.size = 40;
       d.x = this.width/2;
       d.y = this.height/2;
-      d.id = currentLevel + '_' + index++;
       d.level = this.currentLevelIndex;
       d.Name = d.Name || d.Firstname;
       d.data = d;
       this.nodes.push(<Node>d);
+      if(this.currentLevelIndex) { // create link to parent element
+        const link : Link = { source: this.nodes[0], target: this.nodes[this.nodes.length-1], value: 1}
+        this.links.push(<Link>link);
+      }
     });
 
-
-    data.links && data.links.forEach((d) => {
-      this.links.push(<Link>d);
-    });
-
-    this.render();
-
+   
+    this.draw();
 
   }
 
   loadData(url: string) {
 
     d3.queue()
-    .defer(d3.json, url)      
+      .defer(d3.json, url)      
       .await(this.processData.bind(this));
-}
+  }
+
+  
+
+  tooltipFunc(d) {
+    if(!this.tooltipFields) return "";         
+    return this.tooltipFields.map(dd => {return dd + ' : ' + d[dd] }).join("<br>");
+ }
 
   ngOnInit() {
 
-    console.log('D3.js version:', d3['version']);
-
     this.svg = d3.select('svg');    
+    this.svg.attr("oncontextmenu", "return false;")
+        .attr("preserveAspectRatio", "xMinYMin meet")
     this.nodeBase = d3.select('#nodeBase');
     this.linkBase = d3.select('#linkBase');
+    this.tooltip = d3.select("#graph-tooltip");
 
     const sizes = this.svg.node().getBoundingClientRect();
 
@@ -224,7 +288,7 @@ export class AppComponent implements OnInit {
     this.height = sizes.height;
 
     this.simulation = d3.forceSimulation()
-      .force('link', d3.forceLink().id((d: any) => d.id))
+    //  .force('link', d3.forceLink().id((d: any) => d.id))
       .force('charge', d3.forceManyBody())
       .force('collide',d3.forceCollide()
         .strength(1)
